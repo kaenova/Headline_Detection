2b600fb463a3b6bf3d399a31193d09147ff65dd3
Kaenova Mahendra Auditama <kaenova@gmail.com>
2023-02-04 15:39:33 +0700

    feat(lib): adding setup py

---
+++ src/lib/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name = "kaelib",
+    version = "0.1-alpha",
+    author = "Kaenova Mahendra Auditama",
+    author_email = "kaenova@gmail.com",
+    description = "A most used private, hand made library by Kaenova ;)",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.8"
+)
